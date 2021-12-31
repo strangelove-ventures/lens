28246d3d8f543a41f24c8f8442e8236d9584204d
Marko Baricevic <markobaricevic3778@gmail.com>
2021-12-31 15:58:24 +0100

    add staking and distribution

--- cmd/root.go
+++ cmd/root.go
@@ -24,6 +24,20 @@ import (
 	"github.com/spf13/viper"
 )
 
+// TxCommand regesters a new tx command.
+func txCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tx",
+		Short: "query things about a chain",
+	}
+
+	cmd.AddCommand(bankSendCmd())
+	cmd.AddCommand(stakingDelegateCmd())
+	cmd.AddCommand(stakingRedelegateCmd())
+
+	return cmd
+}
+
 var (
 	homePath    string
 	debug       bool
