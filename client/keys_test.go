5ee423dcb6c689166066684205eb14626c659a35
Andrew Gouin <andrew@gouin.io>
2022-01-03 15:36:39 -0700

    Move cosmoshub and osmosis config gen to methods. Add test for key restore

---
+++ client/keys_test.go
@@ -0,0 +1,24 @@
+package client
+
+import (
+	"testing"
+	"github.com/strangelove-ventures/lens/client"
+)
+
+// TestKeyRestore restores a test mnemonic 
+// for a valid return value.
+func TestKeyRestore(t *testing.T) {
+	keyName := "test_key"
+	mnemonic := "blind master acoustic speak victory lend kiss grab glad help demand hood roast zone lend sponsor level cheap truck kingdom apology token hover reunion"
+	expectedAddress := "cosmos15cw268ckjj2hgq8q3jf68slwjjcjlvxy57je2u"
+
+	cl, _ := client.NewChainClient(client.GetCosmosHubConfig("/tmp", true), nil, nil)
+	_ = cl.DeleteKey(keyName) // Delete if test is being run again
+	address, err := cl.RestoreKey(keyName, mnemonic)
+	if err != nil {
+		t.Fatalf("Error while restoring mnemonic: %v", err)
+	}
+	if address != expectedAddress {
+		t.Fatalf("Restored address: %s does not match expected: %s", address, expectedAddress)
+	}
+}
