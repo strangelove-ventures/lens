e01e850b685344832cb244f028508a83bde599f4
jtieri <37750742+jtieri@users.noreply.github.com>
2022-01-06 16:59:16 -0600

    omitempty on trusting-period

--- client/config.go
+++ client/config.go
@@ -21,7 +21,7 @@ type ChainClientConfig struct {
 	KeyringBackend string                  `json:"keyring-backend" yaml:"keyring-backend"`
 	GasAdjustment  float64                 `json:"gas-adjustment" yaml:"gas-adjustment"`
 	GasPrices      string                  `json:"gas-prices" yaml:"gas-prices"`
-	TrustingPeriod string                  `json:"trusting-period" yaml:"trusting-period"`
+	TrustingPeriod string                  `json:"omitempty" yaml:"omitempty"`
 	KeyDirectory   string                  `json:"key-directory" yaml:"key-directory"`
 	Debug          bool                    `json:"debug" yaml:"debug"`
 	Timeout        string                  `json:"timeout" yaml:"timeout"`
