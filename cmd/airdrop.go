2e6e11f32d45e07ea36203a28ed81b5c7ffac0ab
Jack Zampolin <jack.zampolin@gmail.com>
2022-01-28 09:11:23 -0800

    Fix bech32 errors

--- cmd/airdrop.go
+++ cmd/airdrop.go
@@ -6,10 +6,10 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/avast/retry-go"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/spf13/cobra"
-	"github.com/strangelove-ventures/lens/client"
 )
 
 func airdropCmd() *cobra.Command {
@@ -19,7 +19,6 @@ func airdropCmd() *cobra.Command {
 		Long:  "The airdrop file consists of map[string]float64 where the key is the address on the target chain and the value is the amount of coins to be airdropped to that address/1e6 (i.e. atom instead of uatom). The airdrop command 1. checks the addresses in the file to ensure that they are valid for the given chain l",
 		Args:  cobra.RangeArgs(2, 3),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			osmosis, _ := client.NewChainClient(client.GetOsmosisConfig("foo", false), "foo", os.Stdin, os.Stdout)
 			cl := config.GetDefaultClient()
 			keyNameOrAddress := ""
 			if len(args) == 2 {
@@ -45,40 +44,49 @@ func airdropCmd() *cobra.Command {
 				return err
 			}
 
+			denom := args[1]
+			maxSends, err := cmd.Flags().GetInt("max-sends")
+			if err != nil {
+				return err
+			}
+
 			multiMsg := &banktypes.MsgMultiSend{
 				Inputs:  []banktypes.Input{},
 				Outputs: []banktypes.Output{},
 			}
-			amount := sdk.Coin{Denom: args[1], Amount: sdk.NewInt(0)}
+			amount := sdk.Coin{Denom: denom, Amount: sdk.NewInt(0)}
 			var sent int
 			for k, v := range airdrop {
-				to, err := osmosis.DecodeBech32AccAddr(k)
+				to, err := cl.DecodeBech32AccAddr(k)
 				if err != nil {
 					return err
 				}
-				toSendCoin := sdk.NewCoin(args[1], sdk.NewInt(int64(v*1e6)))
+				toSendCoin := sdk.NewCoin(denom, sdk.NewInt(int64(v*1e6)))
 				toSend := sdk.NewCoins(toSendCoin)
 				amount = amount.Add(toSendCoin)
-				multiMsg.Outputs = append(multiMsg.Outputs, banktypes.NewOutput(to, toSend))
+				multiMsg.Outputs = append(multiMsg.Outputs, banktypes.Output{cl.MustEncodeAccAddr(to), toSend})
 				sent += 1
 
-				if len(multiMsg.Outputs) > 300 {
+				if len(multiMsg.Outputs) > maxSends {
 					completion := float64(sent) / float64(len(airdrop))
 					fmt.Printf("(%f) sending %s to %d addresses\n", completion, amount.String(), len(multiMsg.Outputs))
-					multiMsg.Inputs = append(multiMsg.Inputs, banktypes.NewInput(address, sdk.NewCoins(amount)))
-					res, err := cl.SendMsgs(cmd.Context(), []sdk.Msg{multiMsg})
-					if err != nil || res.Code != 0 {
-						return err
-					}
+					multiMsg.Inputs = append(multiMsg.Inputs, banktypes.Input{cl.MustEncodeAccAddr(address), sdk.NewCoins(amount)})
+					retry.Do(func() error {
+						res, err := cl.SendMsgs(cmd.Context(), []sdk.Msg{multiMsg})
+						if err != nil || res.Code != 0 {
+							return err
+						}
+						return nil
+					})
 					multiMsg.Inputs = []banktypes.Input{}
 					multiMsg.Outputs = []banktypes.Output{}
-					amount = sdk.Coin{Denom: args[1], Amount: sdk.NewInt(0)}
+					amount = sdk.Coin{Denom: denom, Amount: sdk.NewInt(0)}
 				}
 			}
 			return nil
 		},
 	}
-	cmd.Flags().Int("max-msgs", 20, "max number of msgs per tx to send")
+	cmd.Flags().Int("max-sends", 200, "max number of msgs per tx to send")
 	return cmd
 }
 
