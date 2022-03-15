02b93c7e2b6d72f981308de807c2a248c67329dd
Mark Rushakoff <mark.rushakoff@gmail.com>
2022-03-15 13:57:25 -0400

    Explicitly pass context to chain registry methods
    Creating a context.Background inside a method is generally an
    anti-pattern. Instead, accept a context as an argument to the method, so
    that a top-level caller can provide a meaningful root context -- in this
    case, a context associated with the *cobra.Command.

--- client/chain_registry/chain_registry.go
+++ client/chain_registry/chain_registry.go
@@ -1,8 +1,10 @@
 package chain_registry
 
+import "context"
+
 type ChainRegistry interface {
-	GetChain(name string) (ChainInfo, error)
-	ListChains() ([]string, error)
+	GetChain(ctx context.Context, name string) (ChainInfo, error)
+	ListChains(ctx context.Context) ([]string, error)
 	SourceLink() string
 }
 
