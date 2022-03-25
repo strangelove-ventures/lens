79dc24dbb2bfd66de09640ca206580ed70bea102
Kyle Moser <moser.kyle@gmail.com>
2022-03-25 00:13:10 -0400

    TxAtHeightRPC - gets TXs successfully but does not decode the proto Messages[]

--- client/query/tx.go
+++ client/query/tx.go
@@ -3,6 +3,7 @@ package query
 import (
 	"fmt"
 
+	"github.com/cosmos/cosmos-sdk/types/query"
 	txTypes "github.com/cosmos/cosmos-sdk/types/tx"
 )
 
@@ -12,7 +13,10 @@ import (
 // RPC endpoint is defined in cosmos-sdk: proto/cosmos/tx/v1beta1/service.proto,
 // See GetTxsEvent(GetTxsEventRequest) returns (GetTxsEventResponse)
 func TxsAtHeightRPC(q *Query, height int64) (*txTypes.GetTxsEventResponse, error) {
-	req := &txTypes.GetTxsEventRequest{Events: []string{"events=" + fmt.Sprintf("%d", height)}}
+	pagination := &query.PageRequest{Limit: 100}
+	orderBy := txTypes.OrderBy_ORDER_BY_UNSPECIFIED
+
+	req := &txTypes.GetTxsEventRequest{Events: []string{"tx.height=" + fmt.Sprintf("%d", height)}, Pagination: pagination, OrderBy: orderBy}
 	return TxsRPC(q, req)
 }
 
