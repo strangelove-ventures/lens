046eb42c8b78df85ed82aa5f0c13c5ee6f49b7ce
justintieri <justin@thetieris.com>
2022-06-07 10:58:30 -0500

    attempt to use the working `ChainClients` configured key in tx simulation

--- client/tx.go
+++ client/tx.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"strings"
 
@@ -179,10 +178,15 @@ func (cc *ChainClient) PrepareFactory(txf tx.Factory) (tx.Factory, error) {
 }
 
 func (cc *ChainClient) CalculateGas(ctx context.Context, txf tx.Factory, msgs ...sdk.Msg) (txtypes.SimulateResponse, uint64, error) {
+	keyInfo, err := cc.Keybase.Key(cc.Config.Key)
+	if err != nil {
+		return txtypes.SimulateResponse{}, 0, err
+	}
+
 	var txBytes []byte
 	if err := retry.Do(func() error {
 		var err error
-		txBytes, err = BuildSimTx(cc.Keybase, txf, msgs...)
+		txBytes, err = BuildSimTx(keyInfo, txf, msgs...)
 		if err != nil {
 			return err
 		}
@@ -280,22 +284,15 @@ type protoTxProvider interface {
 
 // BuildSimTx creates an unsigned tx with an empty single signature and returns
 // the encoded transaction or an error if the unsigned transaction cannot be built.
-func BuildSimTx(keybase keyring.Keyring, txf tx.Factory, msgs ...sdk.Msg) ([]byte, error) {
+func BuildSimTx(info keyring.Info, txf tx.Factory, msgs ...sdk.Msg) ([]byte, error) {
 	txb, err := tx.BuildUnsignedTx(txf, msgs...)
 	if err != nil {
 		return nil, err
 	}
 
 	var pk cryptotypes.PubKey = &secp256k1.PubKey{} // use default public key type
-	if keybase != nil {
-		infos, _ := keybase.List()
-		if len(infos) == 0 {
-			return nil, errors.New("cannot build signature for simulation, key infos slice is empty")
-		}
 
-		// take the first info record just for simulation purposes
-		pk = infos[0].GetPubKey()
-	}
+	pk = info.GetPubKey()
 
 	// Create an empty signature literal as the ante handler will populate with a
 	// sentinel pubkey.
