fa8089ac445a8fdd8e06e8de7d23cd75be965dea
Joe Abbey <joe.abbey@gmail.com>
2022-06-07 04:20:29 -0900

    Register the concrete types on client package init

--- client/keys_test.go
+++ client/keys_test.go
@@ -33,3 +33,40 @@ func TestKeyRestore(t *testing.T) {
 		t.Fatalf("Restored address: %s does not match expected: %s", address, expectedAddress)
 	}
 }
+
+// TestKeyRestoreEth restores a test mnemonic
+func TestKeyRestoreEth(t *testing.T) {
+	keyName := "test_key"
+	mnemonic := "three elevator silk family street child flip also leaf inmate call frame shock little legal october vivid enable fetch siege sell burger dolphin green"
+	expectedAddress := "evmos1dea7vlekr9e34vugwkvesulglt8fx4e457vk9z"
+	var coinType uint32
+	coinType = 60 // Ethereum coin type used in address derivation
+
+	homepath := t.TempDir()
+	cl, err := client.NewChainClient(
+		zaptest.NewLogger(t),
+		&client.ChainClientConfig{
+			Key:            "default",
+			ChainID:        "evmos_9001-2",
+			AccountPrefix:  "evmos",
+			KeyringBackend: "test",
+			GasAdjustment:  1.2,
+			GasPrices:      "0.01uevmos",
+			Timeout:        "20s",
+			OutputFormat:   "json",
+			SignModeStr:    "direct",
+		},
+		homepath, nil, nil,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = cl.DeleteKey(keyName) // Delete if test is being run again
+	address, err := cl.RestoreKey(keyName, mnemonic, coinType)
+	if err != nil {
+		t.Fatalf("Error while restoring mnemonic: %v", err)
+	}
+	if address != expectedAddress {
+		t.Fatalf("Restored address: %s does not match expected: %s", address, expectedAddress)
+	}
+}
