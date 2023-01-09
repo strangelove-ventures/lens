7105fca04458d163b1c726a53d2fa06d24fa3189
colin axnér <25233464+colin-axner@users.noreply.github.com>
2023-01-09 20:14:52 +0100

    chore: bump ibc-go to v6 (#239)
    * chore: bump ibc-go to v6
    
    * chore: go mod tidy to fix botched merge conflicts
    
    Co-authored-by: Justin Tieri <37750742+jtieri@users.noreply.github.com>
    Co-authored-by: jtieri <justin@thetieris.com>

--- client/config.go
+++ client/config.go
@@ -21,8 +21,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/cosmos/cosmos-sdk/x/upgrade"
 	upgradeclient "github.com/cosmos/cosmos-sdk/x/upgrade/client"
-	"github.com/cosmos/ibc-go/v5/modules/apps/transfer"
-	ibc "github.com/cosmos/ibc-go/v5/modules/core"
+	"github.com/cosmos/ibc-go/v6/modules/apps/transfer"
+	ibc "github.com/cosmos/ibc-go/v6/modules/core"
 )
 
 var (
