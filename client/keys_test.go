acaa8812c1e45c6d9b5f2e81a4fc2093360e5cb6
Joe Abbey <joe.abbey@gmail.com>
2023-02-01 16:50:30 -0500

    Add a test case for injective keys

--- client/keys_test.go
+++ client/keys_test.go
@@ -70,3 +70,49 @@ func TestKeyRestoreEth(t *testing.T) {
 		t.Fatalf("Restored address: %s does not match expected: %s", address, expectedAddress)
 	}
 }
+
+// TestKeyRestoreinj restores a test mnemonic
+func TestKeyRestoreInj(t *testing.T) {
+	keyName := "inj_key"
+	mnemonic := "three elevator silk family street child flip also leaf inmate call frame shock little legal october vivid enable fetch siege sell burger dolphin green"
+	expectedAddress := "inj1dea7vlekr9e34vugwkvesulglt8fx4e4uk2udj"
+	var coinType uint32
+	coinType = 60 // Ethereum coin type used in address derivation
+
+	homepath := t.TempDir()
+	cl, err := client.NewChainClient(
+		zaptest.NewLogger(t),
+		&client.ChainClientConfig{
+			Key:            "default",
+			ChainID:        "injective-1",
+			AccountPrefix:  "inj",
+			KeyringBackend: "test",
+			GasAdjustment:  1.2,
+			GasPrices:      "0.01inj",
+			Timeout:        "20s",
+			OutputFormat:   "json",
+			SignModeStr:    "direct",
+			ExtraCodecs:    []string{"injective"},
+		},
+		homepath, nil, nil,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = cl.DeleteKey(keyName) // Delete if test is being run again
+	address, err := cl.RestoreKey(keyName, mnemonic, coinType)
+	if err != nil {
+		t.Fatalf("Error while restoring mnemonic: %v", err)
+	}
+	if address != expectedAddress {
+		t.Fatalf("Restored address: %s does not match expected: %s", address, expectedAddress)
+	}
+	_, err = cl.ListAddresses()
+	if err != nil {
+		t.Fatalf("Error while restoring mnemonic: %v", err)
+	}
+	err = cl.DeleteKey(keyName) // Delete if test is being run again
+	if err != nil {
+		t.Fatalf("Error deleting key: %v", err)
+	}
+}
