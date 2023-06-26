8e3cbe5befd811a3d6d93ab50ae80cd9476627b8
Vitalis Salis <VitSalis@gmail.com>
2023-06-26 21:22:13 +0300

    chore: Cosmos 0.47.3 (#322)
    * chore: Cosmos 0.47.3
    
    * fix

--- client/codecs/ethermint/codec.go
+++ client/codecs/ethermint/codec.go
@@ -21,7 +21,7 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 		&EthAccount{},
 	)
 	registry.RegisterImplementations(
-		(*tx.ExtensionOptionI)(nil),
+		(*tx.TxExtensionOptionI)(nil),
 		&ExtensionOptionsWeb3Tx{},
 		&ExtensionOptionDynamicFeeTx{},
 	)
